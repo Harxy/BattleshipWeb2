164e7fe1b9b910e415cee98cb6b3b782435b8845
mohamed Issaq <minhajraz10@yahoo.com>
2015-06-23 12:18:42 +0100

    passed the first feature test

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -2,8 +2,15 @@ require 'sinatra/base'
 
 class BattleshipsWeb < Sinatra::Base
   get '/' do
-    'Hello BattleshipsWeb!'
+    # 'Hello BattleshipsWeb!'
+    erb :index
+
   end
+  get '/newgame' do
+    @name = params[:name]
+    erb :newgame
+  end
+  set :views, proc {File.join(root, '..', 'views')}
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -3,7 +3,14 @@ require 'spec_helper'
 feature 'Starting a new game' do
   scenario 'Ask the user for their name' do
     visit '/'
-    click_link 'New Game'
-    expect(page).to have_content "What's your name?"
+    fill_in('name', with: 'mohamed')
+    click_button 'New Game'
+    expect(page).to have_content "Hello, mohamed"
+  end
+  scenario 'Ask the user to click a button to start the game and place a ship' do 
+    visit '/newgame'
+    click_button 'Start game'
+    expect(page).to have_content "please enter a coordinate to place your ship"
   end
 end
+
