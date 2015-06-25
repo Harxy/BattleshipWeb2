3e3ec1559b15b274989300ede6b7596a1b4726ad
Paul Harker <harkerp@gmail.com>
2015-06-25 17:36:44 +0100

    done loads of amazing stuff. It's incredible'

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -2,65 +2,67 @@ require 'sinatra/base'
 require 'battleships'
 
 class BattleshipsWeb < Sinatra::Base
-  get '/' do
-    erb :index
-  end
-
-  post '/newgame' do
-    $game = Game.new Player, Board
-    session[:name] = params[:name]
-
-    erb :newgame
-  end
-
-  get '/startgame' do
-    @board = $game.own_board_view $game.player_1
-    erb :startgame
-  end
-
-  get '/place' do
-    @board = $game.own_board_view $game.player_1
-    erb :startgame
-  end
-
-  post '/place' do
-    $game.player_1.place_ship(Ship.send( params[:ship_type]),
-                                         params[:coords],
-                                         params[:direction])
-
-    redirect '/place'
-  end
-
-  post '/start_attacking' do
-
-    erb :start_attacking
-  end
-
-  post '/attack' do
-    @result = $game.player_2.shoot(params[:coords].to_sym)
-    erb :attack
-  end
-
-  set :views, proc {File.join(root, '..', 'views')}
-
-  # start the server if ruby file executed directly
-  run! if app_file == $0
-end
-
---------------------------
-       2 USERS GAME
---------------------------
+#   get '/' do
+#     erb :index
+#   end
+#
+#   post '/newgame' do
+#     $game = Game.new Player, Board
+#     session[:name] = params[:name]
+#
+#     erb :newgame
+#   end
+#
+#   get '/startgame' do
+#     @board = $game.own_board_view $game.player_1
+#     erb :startgame
+#   end
+#
+#   get '/place' do
+#     @board = $game.own_board_view $game.player_1
+#     erb :startgame
+#   end
+#
+#   post '/place' do
+#     $game.player_1.place_ship(Ship.send( params[:ship_type]),
+#                                          params[:coords],
+#                                          params[:direction])
+#
+#     redirect '/place'
+#   end
+#
+#   post '/start_attacking' do
+#
+#     erb :start_attacking
+#   end
+#
+#   post '/attack' do
+#     @result = $game.player_2.shoot(params[:coords].to_sym)
+#     erb :attack
+#   end
+#
+#   set :views, proc {File.join(root, '..', 'views')}
+#
+#   # start the server if ruby file executed directly
+#   run! if app_file == $0
+# end
+#
+# --------------------------
+#        2 USERS GAME
+# --------------------------
+run! if app_file == $0
+ set :views, proc {File.join(root, '..', 'views')}
 
 get '/' do
-    erb :index2
+  erb :index_new
 end
 
 post '/registration' do
-  
-  session[:player] = player_1
-  session[:name  ] = params[:name] 
 
-  erb :newgame2
+  session[:player] = :player_1
+  session[:name  ] = params[:name]
+
+  erb :new_game2
 end
 
 get '/start_new_game' do
@@ -73,13 +75,21 @@ get '/start_new_game' do
 end
 
 get '/join_game' do
-  session[:player]  = player_2
+  session[:player]  = :player_2
   session[:name]    = params[:name]
   @board_2          = $game.own_board_view( $game.player_2 )
   @opponent_board_2 = $game.opponent_board_view $game.player_2
-  
+
   erb :start_game2
 end
-   
+
+post '/start_attacking_player_1'
 
 
+end
+
+post '/start_attacking_player_2'
+
+
+end
+end
