d73ab5f050e812504a6d2c7450730f80484b8b4c
Paul Harker <harkerp@gmail.com>
2015-06-25 15:17:35 +0100

    Done some stuff

--- spec/features/setting_up_the_board_spec.rb
+++ spec/features/setting_up_the_board_spec.rb
@@ -1,12 +1,23 @@
 require 'spec_helper'
 
 feature "setting up the game environment" do
+    before(:each) do
+      $game = Game.new Player, Board
+    end
   scenario "board exist when new game starts" do
-    visit '/'
-    fill_in('name', with: 'mohamed')
-    click_button 'New Game'
-    click_button 'Start game'
+    visit '/startgame'
     expect(page).to have_selector('.board')
     expect(page).to have_content('Please')
+    end
+  scenario "place a ship and send it to the board" do
+
+    visit '/startgame'
+    select 'Submarine', from: 'ship_type'
+    fill_in('coords', with: 'B1')
+    choose 'vertical'
+    click_button 'Place ship'
+    expect(page).to have_content('S')
+
   end
+
 end
